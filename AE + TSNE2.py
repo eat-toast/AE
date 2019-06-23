ef706d195d41154502673b8e204a060dbbeacf6c
toastls93@naver.com <31301664+eat-toast@users.noreply.github.com>
2019-06-23 13:52:16 +0900

    plt.close('all') 수정

--- AE + TSNE2.py
+++ AE + TSNE2.py
@@ -12,17 +12,14 @@ import numpy as np
 lcmap = colors.ListedColormap(['silver', '#FF99FF', '#8000FF', '#0000FF', '#0080FF', '#58FAF4',
                                '#00FF00', '#FFFF00', '#FF8000', '#FF0000', 'darkgreen'])
 
-
 # 미리 만들어둔 모델 불러오기
 from AE_model import encoder, decoder
 
 # 시각화
 from sklearn.manifold import TSNE
 
-
-if not os.path.exists('./AE+TSNE_img'):
-    os.mkdir('./AE+TSNE_img')
-
+if not os.path.exists('./AE+TSNE_img_LeakyReLU'):
+    os.mkdir('./AE+TSNE_img_LeakyReLU')
 
 img_transform = transforms.Compose([
     transforms.ToTensor()
@@ -35,19 +32,16 @@ learning_rate = 1e-3
 
 #  데이터 불러오기
 dataset = MNIST('./data', transform=img_transform)
-dataloader = DataLoader(dataset, batch_size=batch_size , shuffle=True)
-
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 #  모델 설정
 encoder = encoder().cuda()
 decoder = decoder().cuda()
 
-
 #  모델 Optimizer 설정
 criterion = nn.MSELoss()
-encoder_optimizer = torch.optim.Adam( encoder.parameters(), lr=learning_rate, weight_decay=1e-5)
-decoder_optimizer = torch.optim.Adam( decoder.parameters(), lr=learning_rate, weight_decay=1e-5)
-
+encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr=learning_rate, weight_decay=1e-5)
+decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=learning_rate, weight_decay=1e-5)
 
 
 #  fake_image 추가
@@ -57,9 +51,9 @@ decoder_optimizer = torch.optim.Adam( decoder.parameters(), lr=learning_rate, we
 def make_fake_img():
     img_size = 28
     n_fake_img = 60
-    fake_img  = []
+    fake_img = []
     for i in range(n_fake_img):
-        fake_img.append( np.random.randn(img_size * img_size).reshape(1, img_size, img_size) )
+        fake_img.append(np.random.randn(img_size * img_size).reshape(1, img_size, img_size))
 
     fake_img = torch.FloatTensor(fake_img)
     fake_img = fake_img.view(60, 784)
@@ -69,13 +63,12 @@ def make_fake_img():
     return fake_img, fake_label
 
 
-fake_img , fake_label = make_fake_img()
-
+fake_img, fake_label = make_fake_img()
 
 for epoch in range(num_epochs):
     for data in dataloader:
         # <<<<<<<<<<<<<<<<정상 데이터>>>>>>>>>>>>>>>>>>
-        img, label = data  #  label 은 가져오지 않는다.
+        img, label = data  # label 은 가져오지 않는다.
         img = img.view(img.size(0), -1)
         img = Variable(img).cuda()
         # ===================forward=====================
@@ -90,7 +83,7 @@ for epoch in range(num_epochs):
         encoder_optimizer.step()
         decoder_optimizer.step()
     # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}' .format(epoch + 1, num_epochs, float(loss.data) ))
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch + 1, num_epochs, float(loss.data)))
 
     # <<<<<<<<<<<<<<<<비 정상 데이터>>>>>>>>>>>>>>>>>>
     # ===================forward=====================
@@ -106,9 +99,7 @@ for epoch in range(num_epochs):
     encoder_optimizer.step()
     decoder_optimizer.step()
     # ===================log========================
-    print('##### fake epoch [{}/{}], loss:{:.4f} #### '.format(epoch + 1, num_epochs, float(loss.data) ))
-
-
+    print('##### fake epoch [{}/{}], loss:{:.4f} #### '.format(epoch + 1, num_epochs, float(loss.data)))
 
     #  ===================Latent Space 확인========================
     #  6만개의 데이터가 너무 많아서, 1,000개의 sample 만 확인.
@@ -140,8 +131,8 @@ for epoch in range(num_epochs):
     im = ax.scatter(xs, ys, cmap=lcmap, c=latent_label)
     fig.colorbar(im, ax=ax, ticks=range(11))
     ax.set_title('epoch:{}'.format(epoch))
-    fig.savefig('./AE+TSNE_img/' +  str(epoch) + '.jpg')
+    fig.savefig('./AE+TSNE_img_LeakyReLU/' + str(epoch) + '.jpg')
     plt.close('all')
 
-torch.save(encoder.state_dict(), './encoder.pth')
-torch.save(decoder.state_dict(), './decoder.pth')
+torch.save(encoder.state_dict(), './encoder2.pth')
+torch.save(decoder.state_dict(), './decoder2.pth')
